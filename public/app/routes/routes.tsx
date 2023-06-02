d859409cd31f9752216dcd9341202c48baef823f
Josh Hunt <joshhunt@users.noreply.github.com>
2023-06-02 23:02:12 +0100

    testing betterer

--- public/app/routes/routes.tsx
+++ public/app/routes/routes.tsx
@@ -21,7 +21,7 @@ import { SafeDynamicImport } from '../core/components/DynamicImports/SafeDynamic
 import { RouteDescriptor } from '../core/navigation/types';
 import { getPublicDashboardRoutes } from '../features/dashboard/routes';
 
-// Comment 1
+// Comment 2
 export const extraRoutes: RouteDescriptor[] = [];
 
 export function getAppRoutes(): RouteDescriptor[] {
