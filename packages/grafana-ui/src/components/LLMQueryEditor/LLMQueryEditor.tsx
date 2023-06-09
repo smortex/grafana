3767ed0caf8aa38b5ffb89dac1cb5e8bf1eecb0d
Ben Sully <ben.sully@grafana.com>
2023-06-09 14:46:44 +0100

    Add 'vector' services to handle vector search of datasource metadata
    This commit adds two new services to the backend and a ton of associated changes.
    It builds off of the 'provide-metadata-service' branch of the plugin SDK to add
    a 'vector sync' service, which will periodically query for metadata from datasources
    and synchronise them to a configured vector DB for semantic search; and a 'vector'
    service which provides an API for querying for closely related metadata given a
    datasource and some natural language text.
    
    This is then used by the frontend in the LLMQueryEditor component, which now
    accepts a callback which will generate a prompt. Before calling the completions
    endpoint of the LLM with the system and user prompt, it now requests related metadata
    to the user prompt from the backend, and passes this to the callback for the datasource
    frontend to use when constructing the prompt. This allows datasources to include related
    metadata, such as example metrics or queries, in their system prompt.
    
    There are several shortcomings here:
    - only qdrant is supported as a vector database
    - only OpenAI is supported as an LLM
    - it requires a significant commitment to changing the plugin SDK :(
    
    To get this working you'll need to add this to conf/custom.ini:
    
        [llms]
        enabled = true
    
        [llms.vector_db]
        type = "qdrant"
        address = "localhost:6334"
    
    Run this before `make run`:
    
        export GF_LLMS_OPENAI_API_KEY=$OPENAI_API_KEY
    
    And start qdrant in a separate shell:
    
        docker run -p 6333:6333 -p 6334:6334 --rm \
            -v $(pwd)/qdrant_storage:/qdrant/storage \
            qdrant/qdrant

--- packages/grafana-ui/src/components/LLMQueryEditor/LLMQueryEditor.tsx
+++ packages/grafana-ui/src/components/LLMQueryEditor/LLMQueryEditor.tsx
@@ -1,3 +1,5 @@
+import { DataQuery, DataSourceApi } from '@grafana/data';
+import { DataSourceJsonData } from '@grafana/schema';
 import React, { useState } from 'react';
 
 import { Button } from '../Button';
@@ -15,28 +17,69 @@ interface LLMSrvChatCompletionRequest {
   messages: LLMSrvChatMessage[];
 }
 
+interface LLMRelatedMetadataRequest {
+  datasourceType: string;
+  datasourceUid: string;
+  text: string;
+}
+
+type DataSourceMetadata<T extends string> = {
+  [key in T]?: string[];
+}
+
 // We can't import the real interface from @grafana/runtime, but
 // we can use this subset. Hooray for structural typing!
 interface LLMSrv {
   chatCompletions(request: LLMSrvChatCompletionRequest): Promise<string>;
+  relatedMetadata<M extends string>(request: LLMRelatedMetadataRequest): Promise<DataSourceMetadata<M>>;
 }
 
-export interface LLMQueryEditorProps {
+export interface LLMQueryEditorProps<
+  T extends DataSourceApi<Q, O, QIC, M>,
+  M extends string,
+  Q extends DataQuery = DataQuery,
+  O extends DataSourceJsonData = DataSourceJsonData,
+  QIC extends Record<string, object> = {},
+> {
   systemPrompt: string;
+  createPrompt?: (metadata: DataSourceMetadata<M>) => string;
   onChange: (text: string) => void;
+  datasource: DataSourceApi<Q, O, QIC, M>;
   llmSrv: LLMSrv; // Can't import @grafana/runtime here
 }
 
-export function LLMQueryEditor({ systemPrompt, onChange, llmSrv }: LLMQueryEditorProps): JSX.Element {
+export function LLMQueryEditor<
+  T extends DataSourceApi<Q, O, QIC, M>,
+  M extends string,
+  Q extends DataQuery = DataQuery,
+  O extends DataSourceJsonData = DataSourceJsonData,
+  QIC extends Record<string, object> = {},
+>({
+  systemPrompt,
+  createPrompt,
+  onChange,
+  llmSrv,
+  datasource: { uid: datasourceUid, type: datasourceType },
+}: LLMQueryEditorProps<T, M, Q, O, QIC>): JSX.Element {
   const [query, setQuery] = useState('');
   return (
     <div>
       <Form
-        onSubmit={async ({ prompt }) => {
+        onSubmit={async ({ userPrompt }) => {
+          if (createPrompt !== undefined) {
+            const metadata = await llmSrv.relatedMetadata<M>({
+              datasourceUid,
+              datasourceType,
+              text: userPrompt,
+            });
+            console.log(metadata);
+            systemPrompt = createPrompt(metadata);
+          }
+          console.log(systemPrompt);
           const returnedMessage = await llmSrv.chatCompletions({
             messages: [
               { role: 'system', content: systemPrompt },
-              { role: 'user', content: prompt },
+              { role: 'user', content: userPrompt },
             ],
           });
           setQuery(returnedMessage);
@@ -47,7 +90,7 @@ export function LLMQueryEditor({ systemPrompt, onChange, llmSrv }: LLMQueryEdito
           return (
             <>
               <Field>
-                <Input {...register('prompt')} />
+                <Input {...register('userPrompt')} />
               </Field>
               <Button type="submit">Translate</Button>
             </>
